eed3c84473c2d9ee6eb5d746913a47ce2c189530
Ray <rnguyen7388@gmail.com>
2020-08-11 00:47:36 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
     password: "Mapleb0x",
     database: "burgers_db"
